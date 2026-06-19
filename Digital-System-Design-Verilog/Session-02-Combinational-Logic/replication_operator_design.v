3e699102fe3f384abe7aa5427412d7aa4984d0ef
Laiba Khan <laibakhann010@gmail.com>
2026-06-19 23:36:58 +0500

    Delete Digital-System-Design-Verilog directory

--- Digital-System-Design-Verilog/Session-02-Combinational-Logic/replication_operator_design.v
+++
@@ -1,9 +0,0 @@
-module replication_operator (
-    input  logic [1:0] A,
-    output logic [7:0] B
-);
-
-    // Replicates the 2-bit vector A four times to create an 8-bit vector
-    assign B = {4{A}};
-
-endmodule
--- Digital-System-Design-Verilog/Session-03-Multiplexers-and-Arithmetic/two_bit_comparator_design.v
+++
@@ -1,9 +0,0 @@
-module two_bit_comparator (
-    input [1:0] A, B,
-    output eq, neq
-);
-    assign eq = (A == B);
-    assign neq = (A != B);
-  
-endmodule
-
